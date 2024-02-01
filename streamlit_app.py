d4caca7b39917392dae697c87621f87eeff2aada
yash-jain-git <80253453+yash-jain-git@users.noreply.github.com>
2024-02-01 14:42:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
-
-streamlit.title("asdf ghjk")
+streamlit.title('My Parents New Healthy Diner')
+streamlit. header ('Breakfast Menu')
+streamlit. text('Omega 3 & Blueberry Oatmeal')
+streamlit. text('Kale, Spinach & Rocket Smoothie'
+streamit.text ('Hard-Boiled Free-Range Egg')
