ecf51a511d4e336dd2b415e27224b03a51f18954
yash-jain-git <80253453+yash-jain-git@users.noreply.github.com>
2024-02-02 17:03:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.write('The user entered ', fruit_choice)
   else:
